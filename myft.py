37252bb007b1da401dd42b12d362096a1dd919e0
slp <ssruoz@gmail.com>
2022-08-29 23:41:10 +0800

    feat(myft): start implement fastText

---
+++ myft.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import random
+import re, os, sys, joblib
+from collections import defaultdict
+from functools import reduce
+import codefast as cf
+import pandas as pd
+
+
+class fastText(object):
+    def __init__(self) -> None:
+        pass 
+
+    def train_supervised(self, df:pd.DataFrame):
+        pass 
