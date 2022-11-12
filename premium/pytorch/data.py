21a82271c129789e7d1ee1b044cf9a0df5b07d21
LE4PC-MacMini <ssruoz@gmail.com>
2022-11-13 00:35:43 +0800

    feat(spam_bert): add model checkpoint to trainer

--- premium/pytorch/data.py
+++ premium/pytorch/data.py
@@ -39,3 +39,21 @@ def train_test_val_split(df, test_size=0.2, val_size=0.2, random_state=42):
     X, P = train_test_split(df, test_size=test_size, random_state=random_state)
     V, T = train_test_split(P, test_size=val_size, random_state=random_state)
     return X, V, T
+
+
+class AbstractLoader(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+from torch.utils.data import DataLoader
+
+
+class TextLoader(AbstractLoader):
+    def __init__(self, dataset, **kargs):
+        self._DataSet = dataset
+        self.kargs = kargs
+
+    def __call__(self, df: pd.DataFrame, **kargs) -> DataLoader:
+        self.kargs.update(kargs)
+        return DataLoader(self._DataSet(df), **self.kargs)
--- x.py
+++ x.py
@@ -5,3 +5,10 @@ import torch.optim as optim
 
 import codefast as cf 
 
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+if args.save_model:
+    cf.info('save it')
